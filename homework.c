ea81be1e693d82254d9b90a33cb8426725efa972
mathis collot <75610322+mathis123462@users.noreply.github.com>
2022-02-09 18:49:02 +0100

    Add files via upload

--- homework.c
+++ homework.c
@@ -5,27 +5,22 @@
 
 double integrate(double x[3], double y[3], double (*f) (double, double))
 {
-    double I = 0;
-    double xLoc[3];
-    double yLoc[3];
-    double wLoc[3];
-
-    double xi[3] = {1/6,1/6,2/3};
-    double mu[3] = {1/6,2/3,1/6};
-    double w[3] = {1/6,1/6,1/6};
+    double I=0;
+    //double xLoc[3];
+    //double yLoc[3];
+    //double Jaco = cabs((x[1]-x[0])*(y[2]-y[0])-(x[2]-x[0])*(y[1]-y[0]));
+    //double xi[3] = {(double)1/6 , (double)1/6 , (double)2/3 };
+    //double mu[3] = { (double)1/6 ,(double)2/3 ,(double)1/6 };
     
     int n = 3;
-    int i;
-    for (i = 0; i < n; i++) {
-        double phi[3] = { 1 - xi[i]-mu[i],xi[i],mu[i] };
-        for (int j = 0; j < n; j++) {
-            xLoc[i] +=x[j]*phi[j] ;
-            yLoc[i] += y[j] * phi[j];
-            wLoc[i] += w[j] * phi[j];
-        }
-    }
-    for (i = 0; i < n; i++) {
-        I += (wLoc[i] * f(xLoc[i], yLoc[i]));
+    //for (int i = 0; i < n; i++) {
+      //      xLoc[i] =x[0]* (1 - xi[i] - mu[i]) +x[1]* xi[i] +x[2]* mu[i];
+        //    yLoc[i] = y[0] * (1 - xi[i] - mu[i]) + y[1] * xi[i] + y[2] * mu[i];
+    //}
+    
+    double w[3] = { (double) (1 / 6),(double)(1 / 6),(double)(1 / 6) };
+    for (int u = 0; u < n; u++) {
+        I += (w[u] * f(x[u], y[u]));
     }
 //
 // Pour dessiner l'element, les sommets du triangle :-)
@@ -48,28 +43,43 @@ double integrateRecursive(double x[3], double y[3], double (*f)(double,double), 
 // ... A modifier :-)
 // y-compris la ligne juste en dessous :-)
 //
-    double noeudx[3];
-    double noeudy[3];
-    if (n == 0) {
-        double I = integrate(x, y, f);
-        return I;
-    }
-    else {
-        for (int j = 0; j < 3; j++) {
-            noeudx[j] = abs((x[j-1]-x[j])) / 2;
-            noeudy[j] = abs((y[j - 1] - y[j])) / 2;
-        }
-        n--;
-        double x1[3] = {x[0],noeudx[0],noeudx[1]};
-        double y1[3] = { y[0],noeudy[0],noeudy[1]};
-        integrateRecursive(x1, y1, f, n);
-        double x2[3] = { noeudx[0], x[1],noeudx[2] };
-        double y2[3] = { noeudy[0],y[1],noeudy[2] };
-        integrateRecursive(x2,y2, f, n);
-        double x3[3] = { noeudx[1],x[2],noeudx[2] };
-        double y3[3] = { noeudy[1], y[2],noeudy[2] };
-        integrateRecursive(x3, y3, f, n);
-        integrateRecursive(noeudx, noeudy, f, n);
+    double xLoc[3];
+    double yLoc[3];
+    double xi[3] = { (double)(1 / 6) , (double)(1 / 6) , (double)(2 / 3) };
+    double mu[3] = { (double)(1 / 6) ,(double)(2 / 3) ,(double)(1 / 6) };
 
-    }   
+    int m = 3;
+    for (int i = 0; i < m; i++) {
+        xLoc[i] = x[0] * (1 - xi[i] - mu[i]) + x[1] * xi[i] + x[2] * mu[i];
+        yLoc[i] = y[0] * (1 - xi[i] - mu[i]) + y[1] * xi[i] + y[2] * mu[i];
+    }
+    double Jaco = (x[1] - x[0]) * (y[2] - y[0]) - (x[2] - x[0]) * (y[1] - y[0]);
+    
+    double I = 0;
+    if (n > 0) {
+        n--;
+        //double noeudx[3] = {(double)cabs((x[2] - x[0]))/2, (double)cabs((x[0] - x[1])) / 2, (double)cabs((x[1] - x[2])) / 2 };
+        //double noeudy[3] = {(double)cabs((y[2] - y[0])) / 2,(double)cabs((y[0] - y[1])) / 2 ,(double)cabs((y[1] - y[2])) / 2 };
+        double noeudx[3] = { xLoc[0],(double) xLoc[1] / 2 ,(double) xLoc[1] / 2 };
+        double noeudy[3] = { (double)yLoc[2] / 2,(double)yLoc[0] / 2 ,(double)yLoc[2] / 2 };
+
+        
+        double x1[3] = { x[0],noeudx[1],noeudx[0] };
+        double y1[3] = { y[0],noeudy[1],noeudy[0] };
+        double a = integrateRecursive(x1, y1, f, n);
+        double x2[3] = { noeudx[1], x[1],noeudx[2] };
+        double y2[3] = { noeudy[1],y[1],noeudy[2] };
+        double b = integrateRecursive(x2, y2, f, n);
+        double x3[3] = { noeudx[0],x[2],noeudx[2] };
+        double y3[3] = { noeudy[0], y[2],noeudy[2] };
+        double c = integrateRecursive(x3, y3, f, n);
+        double x4[3] = { noeudx[2],noeudx[0],noeudx[1] };
+        double y4[3] = { noeudy[2], noeudy[0],noeudy[1] };
+        double d = integrateRecursive(x4, y4, f, n);
+        return a+b+c+d ;
+    }
+    else{
+        double I = integrate(xLoc, yLoc, f);
+        return I*Jaco;
+    }
 }
